28a0ebceee2817476de7bf9d0947b910d6ce9866
Guilherme Giusti <guilherme.giusti04@gmail.com>
2024-06-13 01:20:15 -0300

    correcao de sintaxe

--- .github/scripts/discord_notifier.py
+++ .github/scripts/discord_notifier.py
@@ -2,6 +2,7 @@ import os
 import requests
 import json
 
+# Configurações do webhook e variáveis de ambiente
 webhook_url = os.getenv('DISCORD_WEBHOOK_URL')
 title = os.getenv('TITLE')
 description = os.getenv('DESCRIPTION')
@@ -13,13 +14,15 @@ workflow_name = os.getenv("WORKFLOW_NAME")
 branch_selected = os.getenv("GITHUB_REF_NAME")
 build_number = os.getenv("WORKFLOW_BUILD")
 
-
+# Conversão de severidade para cor
 severity_colors = {
-    'INFO': 3447003,  # Blue
-    'WARN': 16776960,  # Yellow
-    'ERROR': 15158332  # Red
+    'INFO': 3447003,  # Azul
+    'WARN': 16776960,  # Amarelo
+    'ERROR': 15158332  # Vermelho
 }
+color = severity_colors.get(severity.upper(), 3447003)  # Padrão para Azul se a severidade não for reconhecida
 
+# Campos padrão
 default_fields = [
     {
         "name": "Repository",
@@ -47,8 +50,6 @@ default_fields = [
 additional_fields = json.loads(fields_str) if fields_str else []
 all_fields = default_fields + additional_fields
 
-color = severity_colors.get(severity.upper(), 3447003)  # Default to Blue if severity is not recognized
-
 if not webhook_url:
     raise ValueError("DISCORD_WEBHOOK_URL is not set")
 
@@ -57,7 +58,7 @@ def send_discord_notification(webhook, message_title, desc, fields_list, severit
         "title": message_title,
         "description": desc,
         "color": severity_color,
-        "fields": json.loads(fields_list) if fields_list else []
+        "fields": fields_list
     }
 
     data = {
